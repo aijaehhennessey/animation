53249046ac692d9fa62b255d9aceab3e2bc48a4c
aijaehhennessey <60896810+aijaehhennessey@users.noreply.github.com>
2020-03-29 18:00:42 -0700

    Add files via upload

--- bees.js
+++ bees.js
@@ -14,8 +14,8 @@ function setup() {
 
     let random_bee= { x:random(0,width),
       y:random(0,height),
-      width:35,
-      height:18,
+      width:17.5,
+      height:9,
       vx:random(20,70) ,
       vy:random(10,60),
       color:[random(230,234),random(220,225),random(30,35)],
@@ -28,8 +28,8 @@ function setup() {
       let random_flower={
       x:random(0,width),
       y:random(height/2,height),
-      width:50,
-      height:75,
+      width:25,
+      height:37.5,
       vx:1,
       vy:5}
 flowers.push(random_flower)
@@ -62,15 +62,15 @@ for ( let cloud of clouds) {
   noStroke()
     fill(234, 225, 240)
 
-    circle(cloud.x,cloud.y,400)
-    circle(cloud.x+1,cloud.y+52,400)
-    circle(cloud.x-157,cloud.y+30,400)
-    circle(cloud.x-155,cloud.y+160,400)
-    circle(cloud.x-121,cloud.y+186,400)
-    circle(cloud.x-293,cloud.y+150,200)
-    circle(cloud.x-293,cloud.y+216,250)
-    circle(cloud.x-392,cloud.y+96,250)
-    circle(cloud.x+176,cloud.y+62,350)
+    circle(cloud.x,cloud.y,200)
+    circle(cloud.x+.5,cloud.y+26,200)
+    circle(cloud.x-178.5,cloud.y+30,200)
+    circle(cloud.x-77.5,cloud.y+80,200)
+    circle(cloud.x-60.5,cloud.y+93,200)
+    circle(cloud.x-150,cloud.y+75,100)
+    circle(cloud.x-146,cloud.y+108,125)
+    circle(cloud.x-196,cloud.y+96,125)
+    circle(cloud.x+88,cloud.y+31,175)
 
     cloud.x += cloud.vx + cos(.3)
     cloud.y += cloud.y * cloud.vy
@@ -95,23 +95,23 @@ for ( let cloud of clouds) {
 }
 
     for ( let bee of bees) {
-      strokeWeight(1)
+      strokeWeight(.5)
       fill(bee.color)
       ellipse(bee.x,bee.y,bee.width,bee.height)
       fill(0,0,0)
-      triangle(bee.x-15,bee.y-4,bee.x-17,bee.y+4,bee.x-21,bee.y-2)
+      triangle(bee.x-7.5,bee.y-2,bee.x-8.5,bee.y+2,bee.x-10.5,bee.y-1)
       stroke(0,0,0)
-      strokeWeight(2)
-      line(bee.x-10,bee.y-7,bee.x-10,bee.y+6)
-      line(bee.x-5,bee.y-9,bee.x-5,bee.y+6)
-      line(bee.x+2,bee.y+7,bee.x+2,bee.y-10)
-      line(bee.x+7,bee.y-10,bee.x+8,bee.y+6)
-      line(bee.x+13,bee.y+3,bee.x+12,bee.y-8)
+      strokeWeight(1)
+      line(bee.x-5,bee.y-3.5,bee.x-5,bee.y+3)
+      line(bee.x-2.5,bee.y-4.5,bee.x-2.5,bee.y+3)
+      line(bee.x+1,bee.y+3.5,bee.x+1,bee.y-5)
+      line(bee.x+3.5,bee.y-5,bee.x+4,bee.y+3)
+      line(bee.x+6.5,bee.y+1.5,bee.x+6,bee.y-4)
       strokeWeight(.1)
-      line(bee.x+14,bee.y-4,bee.x+14,bee.y-13)
-      line(bee.x+15,bee.y-4,bee.x+17,bee.y-12)
-      circle(bee.x+17,bee.y-12,2)
-      circle(bee.x+14,bee.y-13,2)
+      line(bee.x+3.5,bee.y-2,bee.x+7,bee.y-6.5)
+      line(bee.x+3.5,bee.y-2,bee.x+8,bee.y-6)
+      circle(bee.x+8,bee.y-6,1)
+      circle(bee.x+7,bee.y-6,1)
 
 
 bee.x += bee.vx * cos(.5)
@@ -139,21 +139,21 @@ bee.y += bee.vy * sin(6)
     for ( let flower of flowers) {
       noStroke()
         fill(27, 69, 28)
-      ellipse(flower.x-2,flower.y+165,5,160)
+      ellipse(flower.x-2,flower.y+165,5,80)
 
       stroke(184, 119, 170)
       strokeWeight(5)
       fill(252, 186, 3)
-      circle(flower.x,flower.y,25)
+      circle(flower.x,flower.y,13)
       fill(217, 139, 108,150)
-        circle(flower.x-50,flower.y,75);
-        circle(flower.x+50,flower.y,75);
-        circle(flower.x,flower.y-50,75);
-        circle(flower.x,flower.y+50,75);
-        circle(flower.x-35,flower.y-35,75);
-        circle(flower.x+35,flower.y-35,75);
-        circle(flower.x-35,flower.y+35,75);
-        circle(flower.x+35,flower.y+35,75);
+        circle(flower.x-25,flower.y,38);
+        circle(flower.x+25,flower.y,38);
+        circle(flower.x,flower.y-25,38);
+        circle(flower.x,flower.y+25,38);
+        circle(flower.x-17,flower.y-17,38);
+        circle(flower.x+17,flower.y-17,38);
+        circle(flower.x-17,flower.y+17,38);
+        circle(flower.x+17,flower.y+17,38);
 
         flower.x += flower.vx
         flower.y += flower.vy + sin(1)
@@ -182,19 +182,19 @@ bee.y += bee.vy * sin(6)
 strokeWeight(20);
 stroke(224, 187, 2)
 fill(230, 179, 41)
-circle(0,0,width/8,)
+circle(0,0,width/16,)
 }
 // a static sun
 
 
 
 textFont('Georgia')
-textSize(75)
+textSize(40)
 strokeWeight(5)
 stroke(194, 118, 155)
 fill(207, 174, 41)
 text("Bees Dont Practice Social Distancing",width/2.5,75)
-textSize(45)
+textSize(25)
 noStroke()
 fill(16, 69, 10)
 text("What form of nature would respond well to the chaos of animation ?:things that came in large quantities, and naturally moved chaoticlly. I eventually considered bees!",width/9,height/9)
